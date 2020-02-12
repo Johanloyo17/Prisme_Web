803570c27ceffec8113330a01d4d814f88ba984d
Johandry <johanloyo6@gmail.com>
2020-02-12 17:13:05 -0400

    dev

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,15 +1,6 @@
 <template>
     <div>
-        <div class="services">
-             <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="User" name="first">User</el-tab-pane>
-                <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-                <el-tab-pane label="bladinmir" name="third">Tu eres loco?</el-tab-pane>
-                <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
-            </el-tabs>
-            
-
-        </div>
+        
     </div>
 </template>
 
